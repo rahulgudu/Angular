1bba28b88aa6eac3300ea53490898f1ea7a1b2ae
Rahul <117711778+rahulgudu@users.noreply.github.com>
2026-03-09 13:01:43 +0000

    feat: Add About component with routing and update navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [TodosComponent],
+  imports: [RouterOutlet, TodosComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { TodosComponent } from './components/todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+];
